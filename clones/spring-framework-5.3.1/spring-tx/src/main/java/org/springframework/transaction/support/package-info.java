5a02b6856ad8441dc48d48f897c75be0a5ad4a86
John McDonald <john@threatrix.io>
2023-04-06 15:11:37 -0500

    update

--- clones/spring-framework-5.3.1/spring-tx/src/main/java/org/springframework/transaction/support/package-info.java
+++ clones/spring-framework-5.3.1/spring-tx/src/main/java/org/springframework/transaction/support/package-info.java
@@ -1,3 +1,26 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://spring.io/projects/spring-framework
+*    Release: https://github.com/spring-projects/spring-framework/releases/tag/v5.2.20.RELEASE
+*    Source File: package-info.java
+*    
+*    Copyrights:
+*      copyright 2002-201/ the original author or authors
+*      copyright (c) 2000-2011 inria, france telecom
+*      copyright 2002-2017 the original author or authors.7
+*      copyright 2002-2017 the original author or authors
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * Support classes for the org.springframework.transaction package.
  * Provides an abstract base class for transaction manager implementations,
