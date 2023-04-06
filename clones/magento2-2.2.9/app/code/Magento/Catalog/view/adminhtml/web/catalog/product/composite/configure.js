16e5ae33b642ef6c7a69fc8bf5cba210f5e0acb5
John McDonald <john@threatrix.io>
2023-04-06 16:00:30 -0500

    update

--- clones/magento2-2.2.9/app/code/Magento/Catalog/view/adminhtml/web/catalog/product/composite/configure.js
+++ clones/magento2-2.2.9/app/code/Magento/Catalog/view/adminhtml/web/catalog/product/composite/configure.js
@@ -1,3 +1,26 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://magento.com
+*    Release: https://github.com/magedin/magento-opensource-releases/releases/tag/2.2.11
+*    Source File: configure.js
+*    
+*    Copyrights:
+*      copyright © magento, inc. all rights reserved
+*    
+*    Licenses:
+*      Open Software License 3.0
+*      SPDXId: OSL-3.0
+*    
+*      Academic Free License v3.0
+*      SPDXId: AFL-3.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
