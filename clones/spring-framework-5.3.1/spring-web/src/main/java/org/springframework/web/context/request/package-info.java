0c21b11d3d1948005a2130c8ab6dd6c8537261a9
John McDonald <john@threatrix.io>
2023-04-06 15:38:45 -0500

    update

--- clones/spring-framework-5.3.1/spring-web/src/main/java/org/springframework/web/context/request/package-info.java
+++ clones/spring-framework-5.3.1/spring-web/src/main/java/org/springframework/web/context/request/package-info.java
@@ -1,3 +1,26 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://spring.io/projects/spring-framework
+*    Release: https://github.com/spring-projects/spring-framework/releases/tag/v5.2.20.RELEASE
+*    Source File: package-info.java
+*    
+*    Copyrights:
+*      copyright 2002-201/ the original author or authors
+*      copyright (c) 2000-2011 inria, france telecom
+*      copyright 2002-2017 the original author or authors.7
+*      copyright 2002-2017 the original author or authors
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * Support for generic request context holding, in particular for
  * scoping of application objects per HTTP request or HTTP session.
