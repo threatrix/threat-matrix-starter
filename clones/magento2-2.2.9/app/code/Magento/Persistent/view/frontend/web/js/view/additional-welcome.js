8de9afc2eb591eeb0a02eddb915f8ee8f30dfdcb
John McDonald <john@threatrix.io>
2023-04-06 14:38:41 -0500

    update

--- clones/magento2-2.2.9/app/code/Magento/Persistent/view/frontend/web/js/view/additional-welcome.js
+++ clones/magento2-2.2.9/app/code/Magento/Persistent/view/frontend/web/js/view/additional-welcome.js
@@ -1,3 +1,25 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/singharti/08-07-19/releases/tag/master-8dcb0b8
+*    Source File: additional-welcome.js
+*    
+*    Copyrights:
+*      copyright © magento, inc. all rights reserved
+*    
+*    Licenses:
+*      Open Software License 3.0
+*      SPDXId: OSL-3.0
+*    
+*      Academic Free License v3.0
+*      SPDXId: AFL-3.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
