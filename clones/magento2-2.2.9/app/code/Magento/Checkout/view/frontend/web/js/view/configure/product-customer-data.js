9eb34872a7cbe363218dbd8461bf9555ffd7771b
John McDonald <john@threatrix.io>
2023-04-06 15:34:31 -0500

    update

--- clones/magento2-2.2.9/app/code/Magento/Checkout/view/frontend/web/js/view/configure/product-customer-data.js
+++ clones/magento2-2.2.9/app/code/Magento/Checkout/view/frontend/web/js/view/configure/product-customer-data.js
@@ -1,3 +1,32 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/fascinosum/magento2/releases/tag/2.3.1
+*    Source File: product-customer-data.js
+*    
+*    Copyrights:
+*      copyright © magento, inc. all rights reserved
+*      copyright (c) 2005 lawrence rosen. permission is granted to copy, distribute, or communicate this license without modificati
+*      copyright © 2005 lawrence rosen. permission is granted to copy, distribute, or communicate this license without modification
+*      copyright (c) 2007, parakey inc
+*      copyright 2017 smartbear software
+*      copyright year from 2017 to 2018. (by @bhargavmehta)
+*      copyright (c) 1991, 1999 free software foundation, inc
+*      (c) 2010-2014 google, inc. http://angularjs.org
+*    
+*    Licenses:
+*      Open Software License 3.0
+*      SPDXId: OSL-3.0
+*    
+*      Academic Free License v3.0
+*      SPDXId: AFL-3.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 require([
     'jquery',
     'Magento_Customer/js/customer-data',
