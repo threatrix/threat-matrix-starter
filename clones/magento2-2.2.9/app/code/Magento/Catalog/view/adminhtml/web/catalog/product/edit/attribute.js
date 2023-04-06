7b625b98eae9877a2d718eb66de5b4ae30a43c9b
John McDonald <john@threatrix.io>
2023-04-06 16:22:23 -0500

    update

--- clones/magento2-2.2.9/app/code/Magento/Catalog/view/adminhtml/web/catalog/product/edit/attribute.js
+++ clones/magento2-2.2.9/app/code/Magento/Catalog/view/adminhtml/web/catalog/product/edit/attribute.js
@@ -1,3 +1,22 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/Sater1917/M2_F1/releases/tag/main-5c9fe28
+*    Source File: attribute.js
+*    
+*    Copyrights:
+*      copyright © magento, inc. all rights reserved
+*    
+*    Licenses:
+*      Open Software License 3.0
+*      SPDXId: OSL-3.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
