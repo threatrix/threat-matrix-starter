b9162a2cae76eda0d236622cd4d3ee238bbdbc6c
John McDonald <john@threatrix.io>
2023-04-06 15:20:33 -0500

    update

--- clones/spring-framework-5.3.1/spring-core/src/main/java/org/springframework/objenesis/package-info.java
+++ clones/spring-framework-5.3.1/spring-core/src/main/java/org/springframework/objenesis/package-info.java
@@ -1,3 +1,26 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://spring.io/projects/spring-framework
+*    Release: https://github.com/spring-projects/spring-framework/releases/tag/v5.3.5
+*    Source File: package-info.java
+*    
+*    Copyrights:
+*      copyright 2002,2003,2004 the apache software foundation
+*      copyright 2003,2004 the apache software foundation
+*      copyright (c) 2000-2011 inria, france telecom
+*      copyright 2002-2018 the original author or authors
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * Spring's repackaging of
  * <a href="http://objenesis.org">Objenesis 3.0</a>
