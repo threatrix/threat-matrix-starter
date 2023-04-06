7c1f374b6b9baf33f440f2665040a4f18a3516d2
John McDonald <john@threatrix.io>
2023-04-06 14:25:27 -0500

    update

--- clones/magento2-2.2.9/lib/web/tiny_mce/classes/html/Writer.js
+++ clones/magento2-2.2.9/lib/web/tiny_mce/classes/html/Writer.js
@@ -1,3 +1,25 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/allininfosystems/wvodesign/releases/tag/master-ab0f8f0
+*    Source File: Writer.js
+*    
+*    Copyrights:
+*      copyright 2010, moxiecode systems ab
+*    
+*    Licenses:
+*      Open Software License 3.0
+*      SPDXId: OSL-3.0
+*    
+*      Academic Free License v3.0
+*      SPDXId: AFL-3.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * Writer.js
  *
