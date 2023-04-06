c031385f71187fa0eda263801b4beb93a980aa2a
John McDonald <john@threatrix.io>
2023-04-06 14:45:15 -0500

    update

--- clones/spring-framework-5.3.1/spring-core/src/main/java/org/springframework/core/io/buffer/DefaultDataBuffer.java
+++ clones/spring-framework-5.3.1/spring-core/src/main/java/org/springframework/core/io/buffer/DefaultDataBuffer.java
@@ -1,4 +1,24 @@
 /*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://spring.io/projects/spring-framework
+*    Release: https://github.com/spring-projects/spring-framework/releases/tag/v5.2.18.RELEASE
+*    Source File: DefaultDataBuffer.java
+*    
+*    Copyrights:
+*      copyright 2002-2020 the original author or authors
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
+/*
  * Copyright 2002-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
