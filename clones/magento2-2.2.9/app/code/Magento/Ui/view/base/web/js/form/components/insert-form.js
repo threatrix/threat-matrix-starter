b122def60cc2c0f3705455e61bf6a01f81ca7141
John McDonald <john@threatrix.io>
2023-04-06 16:38:08 -0500

    update

--- clones/magento2-2.2.9/app/code/Magento/Ui/view/base/web/js/form/components/insert-form.js
+++ clones/magento2-2.2.9/app/code/Magento/Ui/view/base/web/js/form/components/insert-form.js
@@ -1,3 +1,22 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/K4milo/doble-dos-src/releases/tag/master-19bb57c
+*    Source File: insert-form.js
+*    
+*    Copyrights:
+*      copyright © magento, inc. all rights reserved
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
