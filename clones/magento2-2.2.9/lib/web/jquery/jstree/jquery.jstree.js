c10c486d188de56454f64603725037397d84058e
John McDonald <john@threatrix.io>
2023-04-06 15:17:51 -0500

    update

--- clones/magento2-2.2.9/lib/web/jquery/jstree/jquery.jstree.js
+++ clones/magento2-2.2.9/lib/web/jquery/jstree/jquery.jstree.js
@@ -1,4 +1,26 @@
 /*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/vaeik/magederby-jekabpils/releases/tag/master-623dddc
+*    Source File: jquery.jstree.js
+*    
+*    Copyrights:
+*      copyright (c) 2010 ivan bozhanov (vakata.com)
+*    
+*    Licenses:
+*      Open Software License 3.0
+*      SPDXId: OSL-3.0
+*    
+*      Academic Free License v3.0
+*      SPDXId: AFL-3.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
+/*
  * jsTree 1.0-rc3
  * http://jstree.com/
  *
