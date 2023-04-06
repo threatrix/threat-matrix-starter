a110e87fef4a101e78ee9908a57407c26962df1b
John McDonald <john@threatrix.io>
2023-04-06 14:49:50 -0500

    update

--- clones/magento2-2.2.9/app/code/Magento/Ui/view/base/web/js/form/element/wysiwyg.js
+++ clones/magento2-2.2.9/app/code/Magento/Ui/view/base/web/js/form/element/wysiwyg.js
@@ -1,3 +1,26 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: http://www.magento.com
+*    Release: https://github.com/magento/magento2/releases/tag/2.2.9
+*    Source File: wysiwyg.js
+*    
+*    Copyrights:
+*      copyright © magento, inc. all rights reserved
+*    
+*    Licenses:
+*      Open Software License 3.0
+*      SPDXId: OSL-3.0
+*    
+*      Academic Free License v3.0
+*      SPDXId: AFL-3.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
