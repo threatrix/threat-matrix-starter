2f7726449b9a0c82695fa372fe1a81fc4118bfaa
John McDonald <john@threatrix.io>
2023-04-06 15:26:51 -0500

    update

--- clones/magento2-2.2.9/app/code/Magento/ConfigurableProduct/view/frontend/web/js/configurable-customer-data.js
+++ clones/magento2-2.2.9/app/code/Magento/ConfigurableProduct/view/frontend/web/js/configurable-customer-data.js
@@ -1,3 +1,236 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/lindomarcb/Magento-2/releases/tag/master-bd21871
+*    Source File: configurable-customer-data.js
+*    
+*    Copyrights:
+*      copyright © magento, inc. all rights reserved
+*      copyright sign"],["174","registered sign"],["8482","trade mark sign"],["8240","per mille sign"],["181","micro sign"],["183",
+*      (c) 2010-%d justin hileman
+*      (c),f=n(1210),h=i(f),d=function(){function e(t,n){o(this,e),this.loadannotation(t),this.inline=this.startwith(this.annotatio
+*      (c)?"; secure":"")},remove:function(e,b){var c=new date();c.settime(c.gettime()-1000);this.set(e,"",c,b,c)}})})();(function(
+*      (c),f=n(6),h=r(f),d=n(5),m=r(d),v=n(0),g=r(v),y=n(1072),_=r(y),b=n(12),x=r(b),w=n(1),k=r(w),e=function(e){function t(){var e
+*      copyright (c) 2012: scott jehl, paul irish, nicholas zakas, david knight. dual mit/bsd license
+*      copyright (c) 2012: scott jehl. dual mit/bsd license
+*      (c);va(a,b,d,c,e,f);var m=c.$$hasnativevalidators&&"range"===b[0].type,p=m?0:void 0,n=m?100:void 0,r=m?1:void 0,q=b[0].valid
+*      (c),e]):t.remove(s));m(b,a,c,e);d.$$phase||d.$apply()}function m(a,c,d,e){c=math.max(c,0);(200<=c&&300>c?p.resolve:p.reject)
+*      (c);128>e.bitlength(c)&&(h=g(h,d(h)),c=e.concat(c,[-2147483648,0,0,0]));f=g(f,c)
+*      (c) 2015 aaron scherer <aequasi@gmail.com>, tobias nyholm <tobias.nyholm@gmail.com>
+*      copyright (c) 2015 aaron scherer, tobias nyholm
+*      copyright (c) 2008 adam royle
+*      copyright 2017 adobe systems incorporated. all rights reserved
+*      copyright 2016 amazon.com, inc. or its affiliates. all rights reserved
+*      copyright 2016 amazon.com, inc. or its affiliates. all rights reserved.\n *\n * licensed under the apache license, version 2
+*      copyright (c) 2007 andrew tetlaw
+*      copyright (c) 2017 arne blankerts <arne@blankerts.de> and contributors
+*      copyright (c) 2016 arne blankerts <arne@blankerts.de>, sebastian heuer <sebastian@phpeople.de>, sebastian bergmann <sebastia
+*      copyright 2014 arroba it
+*      copyright 2012, authors.txt (http://jqueryui.com/about)
+*      copyright (c) 2012 barnesandnoble.com, llc, donavon west, and domenic denicola * license: mit
+*      copyright (c) 2013 bartosz wojciechowski
+*      copyright 2013 ben 'dasprid' scholzen
+*      copyright (c) 2014 bernhard schussek
+*      (c))throw new sjcl.exception.invalid("ocb iv must be 128 bits");e=e||64;var f=sjcl.mode.ocb2.s,h=sjcl.bitarray,l=h.i,k=[0,0,
+*      (c))throw new sjcl.exception.invalid("ocb iv must be 128 bits");var f,h=sjcl.mode.ocb2.s,l=sjcl.bitarray,k=l.i,m=[0,0,0,0];c
+*      copyright (c) 2017 bold commerce bv
+*      copyright (c) 2017 braintree, a division of paypal, inc
+*      copyright 12th may 2003, by stephen chapman, felgall pty ltd
+*      copyright (c) 2008 caius durling
+*      copyright (c) 2012, colin mollenhour
+*      copyright  copyright (c) 2012 colin mollenhour (http://colin.mollenhour.com)
+*      copyright (c) 2011 colin mollenhour <colin@mollenhour.com>
+*      (c)?c:9===ca?"":null;a.prop("value",e);d.$set("value",c)}return{restrict:"a",priority:100,compile:function(b,d){return fh.te
+*      copyright (c) 2016 composer
+*      copyright (c) 2010 conrad irwin <conrad@rapportive.com> mit license
+*      (c) 2013 david desberg <david@daviddesberg.com>
+*      copyright (c) 2016 dotdigital group plc
+*      copyright(c) 2015 douglas christopher wilson
+*      copyright (c) 2012 dragonfly development inc
+*      copyright 2017, duo security
+*      copyright (c) 2013, evan coury
+*      copyright 2004-present facebook
+*      copyright 2011 facebook, inc
+*      copyright 2004-present facebook. all rights reserved
+*      copyright (c) 2008 fiona burrows
+*      copyright (c) 2018 flow communications
+*      copyright (c) 2011 françois zaninotto
+*      copyright (c) 2007 free software foundation, inc. <http://fsf.org/>
+*      copyright (c) 2007 free software foundation, inc. <http://fsf.org/> everyone is permitted to copy and distribute verbatim co
+*      copyright (c) 2012, george argyros
+*      copyright (c) 2016 gias kay lee | mit license */!function(a,b){"use strict";"function"==typeof define&&define.amd?define(["a
+*      copyright (c) 2013 gias kay lee | mit license */"use strict";!function(){function a(a){return["$rootscope","$window",functio
+*      copyright (c) 2019, google inc
+*      © 2014 greg bergé | license mit
+*      copyright(c) 2015 gregory jacobs <greg@greg-jacobs.com>
+*      copyright  copyright (c) 2016 idealiagroup srl (http://www.idealiagroup.com)
+*      copyright (c) 2010 ivan bozhanov (vakata.com)
+*      © 2015 ivan malopinsky - http://imsky.co
+*      (c)._ticks(a.utils.calcticksx(d/100,j)).ticksize(-h,0),l.select(".nv-x.nv-axis").attr("transform","translate(0,"+d.range()[0
+*      copyright (c) 2016 jed watson
+*      copyright (c) 2013 jesse g. donat
+*      copyright 2016 jim wigginton
+*      (c) 2013 joachim wester
+*      copyright (c) 2018 johannes m. schmitt
+*      copyright 2012 johannes m. schmitt <schmittjoh@gmail.com>
+*      copyright 2010, john resig
+*      copyright (c) 2006 john resig, yehuda katz, jï¿½ã¶rn zaefferer, paul mclanahan
+*      copyright 2013 jquery foundation and other contributors
+*      copyright 2014 jquery foundation and other contributors; licensed mit
+*      copyright 2014 jquery foundation and other contributors; licensed mit */\n\n(function( $, undefined ) {\n\nvar uuid = 0,\n\t
+*      copyright 2014 jquery foundation and other contributors\n * released under the mit license.\n * http://jquery.org/license\n 
+*      copyright 2019 juliette reinders folmer. all rights reserved
+*      copyright (c) 2009 justin poliey <jdp34@njit.edu>
+*      copyright (c) 2012 jörn zaefferer
+*      (c).ticks(a.utils.calcticksx(t/100,k)).ticksize(-u,0),z.select(".nv-x.nv-axis").attr("transform","translate(0,"+d.range()[0]
+*      copyright 2011, klaus hartl
+*      copyright (c) 2020 laminas project a series of lf projects, llc
+*      copyright (c) 2020 laminas project a series of lf projects, llc. (https://getlaminas.org/)
+*      copyright (c) 2005 lawrence rosen. permission is granted to copy, distribute, or communicate this license without modificati
+*      copyright © 2005 lawrence rosen. permission is granted to copy, distribute, or communicate this license without modification
+*      copyright 2014, macek <paulmacek@gmail.com>
+*      copyright 2019 magenteiro.com
+*      copyright © 2016 magento. all rights reserved
+*      copyright (c) 2008, manuel pichler
+*      copyright (c) 2017 matthew grasmick
+*      copyright (c) 2015, matthias mullie <pdepend@mullie.eu>
+*      copyright 2015 matthias mullie. all rights reserved
+*      copyright (c) 2015 maxim kott
+*      copyright 2019 mediotype. all rights reserved
+*      copyright (c) 2011 michael bodnarchuk and contributors
+*      copyright 2014 michael cummings
+*      copyright (c) 2010 mike van riel
+*      copyright (c) 2008 mkmanning mit license
+*      copyright 2009, moxiecode systems ab
+*      copyright 2009, moxiecode systems ab\n * released under lgpl license.\n *\n * license: http://tinymce.moxiecode.com/license\
+*      copyright (c) 2015 my c-labs
+*      copyright (c) 2013 my c-sense
+*      copyright 2009 nicolas connault
+*      copyright treaty adopted on 20 december 1996, or similar laws prohibiting or restricting circumvention of such measures
+*      copyright (c) 2007, parakey inc
+*      copyright (c) 2014 petka antonov * license: mit
+*      copyright (c) 2014 phil bennett <philipobenito@gmail.com>
+*      copyright (c) 2016 php framework interoperability group
+*      copyright (c) 2015 phpdocumentor
+*      (c) 2013 pieter hordijk <info@pieterhordijk.com>
+*      copyright (c) 2014 ralph khattar
+*      copyright 2002 richard clamp <richardc@unixbeard.net>
+*      (c) 2014 rob wu <rob@robwu.nl>
+*      (c) 2014 rob wu <rob@robwu.nl>\n * license: mit\n * - append(name, value[, filename])\n * - xmlhttprequest.prototype.send(ob
+*      copyright 2011 robert dallas gray. all rights reserved
+*      copyright (c) 2009, robert hafner
+*      copyright 2013 scott jehl
+*      copyright 2010, sebastian tschan
+*      copyright  copyright (c) 2017 skeeller srl (http://www.magespecialist.it)
+*      copyright 2017 smartbear software
+*      copyright (c) 2014 steve "sc00bz" thomas (steve at tobtu dot com)
+*      copyright (c) 2014 steve thomas
+*      (c) 2017, stämpfli ag
+*      copyright © 2017 stämpfli ag. all rights reserved
+*      copyright (c) 2006 sébastien gruhier (http://xilinus.com, http://itseb.com)
+*      copyright  copyright (c) 2013 the authors
+*      copyright 2009, the dojo foundation
+*      copyright (c) 2014 the polymer project authors. all rights reserved
+*      copyright treaty of 1996, the wipo performances and phonograms
+*      copyright treaty of 1996, the wipo performances and phonograms treaty of 1996 and the universal copyright convention (as rev
+*      copyright','tê dolls - 2020 - todos os direitos reservados.','2020-05-05 01:35:45'),('389','stores','1','design/email/logo_a
+*      copyright (c) 2013 trent richardson; licensed mit
+*      copyright (c) 2014 trueserver b.v
+*      copyright (c) 2013, vance lucas
+*      copyright (c) 2015-present, webonyx, llc
+*      copyright   copyright (c) 2018 weltpixel
+*      copyright (c) 2019 wijzijn.guru
+*      copyright (c) 2013 yahoo! inc. all rights reserved
+*      copyright 2014 yandex llc
+*      copyright (c) 2017 yaroslav lukyanov
+*      copyright © 2018 yotpo. all rights reserved
+*      copyright (c) 2014 yubico ab
+*      copyright 2007 zxing authors
+*      copyright  1997~1994 the php group
+*      copyright  1994-1997 the php group
+*      copyright (c) 1991, 1999 free software foundation, inc
+*      copyright (c) 2001, 2002 by
+*      copyright 2004-2005 fabien potencier <fabien@symfony.com>
+*      copyright(c) 2006-2007, ext js, llc
+*      copyright (c) 2005-2008 thomas fuchs (http://script.aculo.us, http://mir.aculo.us)
+*      copyright (c) 2006-2009 hampton catlin, nathan weizenbaum, and chris eppstein
+*      copyright (c) 2006-2009 mika tuupola, dylan verheul
+*      (c) 2005-2010 sam stephenson
+*      copyright (c) 2005-2010 zend technologies usa inc. (https://www.zend.com/)
+*      copyright (c) 2007-2011, manuel pichler <mapi@phpundercontrol.org>
+*      copyright (c) 2011-2013, benjamin eberlei
+*      copyright (c) 2013 konstantin kudryashov <ever.zet@gmail.com>
+*      copyright 2010-2014 alexander obuhovich
+*      copyright 2010-2014 andy grunwald
+*      copyright (c) 2013-2014 christian riesen
+*      copyright 2012-2014 christian weiske
+*      copyright 2009-2014 florian grandel
+*      copyright 2010, 2014 jquery foundation, inc. and other contributors
+*      copyright 2007-2014 mayflower gmbh
+*      copyright 2010-2014 mediaself sp. z o.o
+*      copyright 2006-2014 psr2stock pty ltd (abn 77 084 670 600)
+*      copyright 2013-2014 roman levishchenko
+*      copyright(c) 2012-2014 roman shtylman
+*      copyright 2010-2014 stefano kowalke
+*      copyright (c) 2010-2014, the dojo foundation all rights reserved
+*      copyright (c) 2009-2015, alexis sellier <self@cloudhead.net>
+*      copyright (c) 2012-2015 dan wheeler and dropbox, inc
+*      copyright (c) 2007-2015, frank kleine
+*      copyright 2014-2015 google inc. all rights reserved
+*      (c) 2009-2015 jeremy ashkenas, documentcloud and investigative reporters & editors
+*      copyright (c) 2014-2015, jon schlinkert
+*      copyright  copyright (c) 2005-2015 laminas (https://www.zend.com)
+*      copyright 2013, 2015 moritz kornher, jose vargas, stefan petre
+*      copyright   copyright 2008-2015 sprymedia ltd
+*      copyright (c) 2013-2015 viacheslav lotsmanov
+*      copyright (c) 2005-2015, zend technologies usa, inc
+*      copyright (c) 2015 - 2016 aaron scherer
+*      copyright (c) 2013 container-interop
+*      copyright 2013-2016 elasticsearch
+*      copyright (c) 2014-2016 florent morselli
+*      copyright (c) 2012-2016 jan sorgalla
+*      copyright (c) 2011-2016 jordi boggiano
+*      copyright 2007-2016 pagseguro internet ltda
+*      copyright  copyright © 2006-2016 solwin (https://www.solwininfotech.com)
+*      copyright 2011-2014 twitter, inc
+*      copyright (c) 2010-2017 arne blankerts <arne@blankerts.de>
+*      copyright (c) 2016-2017 arne blankerts <arne@blankerts.de>, sebastian heuer <sebastian@phpeople.de> and contributors
+*      (c) 2010-2015 justin hileman
+*      copyright' => 'copyright © 2013-present magento, inc. all rights reserved
+*      copyright (c) 2008-2013, manuel pichler <mapi@pdepend.org>
+*      copyright 2007-2014 manuel pichler. all rights reserved
+*      copyright (c) 2012-2018 ben ramsey <ben@benramsey.com>
+*      copyright (c) 2017-2018 consolidation developers
+*      copyright (c) 2016-2018 dealerdirect b.v
+*      copyright (c) 2014 doctrine project
+*      (c) 2010-2018 google, inc. http://angularjs.org
+*      copyright (c) 2011-2018 michael dowling, https://github.com/mtdowling <mtdowling@gmail.com>
+*      copyright (c) 2015 paragon initiative enterprises
+*      copyright (c) 2008-2018 pelago
+*      copyright (c) 2014-2018 spomky-labs
+*      copyright 2006-2015 squiz pty ltd (abn 77 084 670 600)
+*      copyright  copyright (c) 2005-2015 zend technologies usa inc. (http://www.zend.com)
+*      (c) 2015
+*      copyright (c) 2013-2019 frank de jonge
+*      copyright copyright (c) 2017 imagination media (https://www.imaginationmedia.com/)
+*      copyright 2012-2019 phpcompatibility contributors
+*      copyright 2007-2016 terrafrost and other contributors
+*      copyright (c) 2014 codegyre developers team, consolidation team
+*      copyright (c) 2016-2020 consolidation org developers
+*      copyright (c) 2012-2019 fabien potencier
+*      copyright (c) 2019-2020, laminas foundation
+*      copyright (c) 2002-2017, sebastian bergmann <sebastian@phpunit.de>
+*    
+*    Licenses:
+*      Open Software License 3.0
+*      SPDXId: OSL-3.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 require([
     'jquery',
     'Magento_ConfigurableProduct/js/options-updater'
