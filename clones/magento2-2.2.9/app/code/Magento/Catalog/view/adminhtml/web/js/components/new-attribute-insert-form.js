f56f74ad696e1cfebaae7fb305d3eeb8cc036b05
John McDonald <john@threatrix.io>
2023-04-06 15:23:43 -0500

    update

--- clones/magento2-2.2.9/app/code/Magento/Catalog/view/adminhtml/web/js/components/new-attribute-insert-form.js
+++ clones/magento2-2.2.9/app/code/Magento/Catalog/view/adminhtml/web/js/components/new-attribute-insert-form.js
@@ -1,3 +1,25 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/magento-2/magento-2-community-sample-data/releases/tag/2.1.16
+*    Source File: new-attribute-insert-form.js
+*    
+*    Copyrights:
+*      copyright © magento, inc. all rights reserved
+*    
+*    Licenses:
+*      Open Software License 3.0
+*      SPDXId: OSL-3.0
+*    
+*      Academic Free License v3.0
+*      SPDXId: AFL-3.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
