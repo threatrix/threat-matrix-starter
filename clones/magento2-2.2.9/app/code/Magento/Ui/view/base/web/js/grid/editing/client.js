21546cb2bb92db249147c7253f9491817d6495ff
John McDonald <john@threatrix.io>
2023-04-06 15:25:47 -0500

    update

--- clones/magento2-2.2.9/app/code/Magento/Ui/view/base/web/js/grid/editing/client.js
+++ clones/magento2-2.2.9/app/code/Magento/Ui/view/base/web/js/grid/editing/client.js
@@ -1,3 +1,25 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/ThanhQuang1410/e-commerce-website-for-multi-provider/releases/tag/master-5386b17
+*    Source File: client.js
+*    
+*    Copyrights:
+*      copyright © magento, inc. all rights reserved
+*    
+*    Licenses:
+*      Open Software License 3.0
+*      SPDXId: OSL-3.0
+*    
+*      Academic Free License v3.0
+*      SPDXId: AFL-3.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
