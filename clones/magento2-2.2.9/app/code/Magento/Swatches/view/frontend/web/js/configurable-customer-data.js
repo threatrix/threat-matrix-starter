44f601c76c2e68f074a5e0636a3236dc0439f606
John McDonald <john@threatrix.io>
2023-04-06 15:07:11 -0500

    update

--- clones/magento2-2.2.9/app/code/Magento/Swatches/view/frontend/web/js/configurable-customer-data.js
+++ clones/magento2-2.2.9/app/code/Magento/Swatches/view/frontend/web/js/configurable-customer-data.js
@@ -1,3 +1,25 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/uzair90/versatbales/releases/tag/master-ac9070b
+*    Source File: configurable-customer-data.js
+*    
+*    Copyrights:
+*      copyright © magento, inc. all rights reserved
+*    
+*    Licenses:
+*      Open Software License 3.0
+*      SPDXId: OSL-3.0
+*    
+*      Academic Free License v3.0
+*      SPDXId: AFL-3.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
