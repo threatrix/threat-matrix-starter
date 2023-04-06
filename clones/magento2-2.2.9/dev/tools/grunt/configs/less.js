8eab1625793493649533771fcf7903e1ebbbbfcc
John McDonald <john@threatrix.io>
2023-04-06 14:32:58 -0500

    update

--- clones/magento2-2.2.9/dev/tools/grunt/configs/less.js
+++ clones/magento2-2.2.9/dev/tools/grunt/configs/less.js
@@ -1,3 +1,22 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/schoolofnetcom/magento-templates/releases/tag/master-6734933
+*    Source File: less.js
+*    
+*    Copyrights:
+*      copyright © magento, inc. all rights reserved
+*    
+*    Licenses:
+*      MIT License
+*      SPDXId: MIT
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
