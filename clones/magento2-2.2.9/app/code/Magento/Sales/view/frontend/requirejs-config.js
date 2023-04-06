860cc95a80027faaa6013b90ed985c10d1a6ee98
John McDonald <john@threatrix.io>
2023-04-06 15:06:05 -0500

    update

--- clones/magento2-2.2.9/app/code/Magento/Sales/view/frontend/requirejs-config.js
+++ clones/magento2-2.2.9/app/code/Magento/Sales/view/frontend/requirejs-config.js
@@ -1,3 +1,26 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: http://54.162.111.103/magento2/
+*    Release: https://github.com/manojkmishra/dowell_magento2_aws/releases/tag/master-6f3d490
+*    Source File: requirejs-config.js
+*    
+*    Copyrights:
+*      copyright © magento, inc. all rights reserved
+*    
+*    Licenses:
+*      Open Software License 3.0
+*      SPDXId: OSL-3.0
+*    
+*      Academic Free License v3.0
+*      SPDXId: AFL-3.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
