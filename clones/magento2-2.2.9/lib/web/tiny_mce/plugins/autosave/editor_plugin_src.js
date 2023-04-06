e072aec2fdf565790d9da3c9c8879e9ba0503d4d
John McDonald <john@threatrix.io>
2023-04-06 15:59:39 -0500

    update

--- clones/magento2-2.2.9/lib/web/tiny_mce/plugins/autosave/editor_plugin_src.js
+++ clones/magento2-2.2.9/lib/web/tiny_mce/plugins/autosave/editor_plugin_src.js
@@ -1,3 +1,26 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://magento.com
+*    Release: https://github.com/magedin/magento-opensource-releases/releases/tag/2.4.2-p2
+*    Source File: editor_plugin_src.js
+*    
+*    Copyrights:
+*      copyright 2009, moxiecode systems ab
+*    
+*    Licenses:
+*      Open Software License 3.0
+*      SPDXId: OSL-3.0
+*    
+*      Academic Free License v3.0
+*      SPDXId: AFL-3.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * editor_plugin_src.js
  *
