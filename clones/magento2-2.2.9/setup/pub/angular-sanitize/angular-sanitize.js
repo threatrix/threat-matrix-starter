7b63bc26379ea9eff1dd0213158cc2f4a49a7bae
John McDonald <john@threatrix.io>
2023-04-06 16:13:13 -0500

    update

--- clones/magento2-2.2.9/setup/pub/angular-sanitize/angular-sanitize.js
+++ clones/magento2-2.2.9/setup/pub/angular-sanitize/angular-sanitize.js
@@ -1,3 +1,25 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/chandra-insync/testmagento/releases/tag/master-d751136
+*    Source File: angular-sanitize.js
+*    
+*    Copyrights:
+*      (c) 2010-2018 google, inc. http://angularjs.org
+*    
+*    Licenses:
+*      Open Software License 3.0
+*      SPDXId: OSL-3.0
+*    
+*      Academic Free License v3.0
+*      SPDXId: AFL-3.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * @license AngularJS v1.6.9
  * (c) 2010-2018 Google, Inc. http://angularjs.org
