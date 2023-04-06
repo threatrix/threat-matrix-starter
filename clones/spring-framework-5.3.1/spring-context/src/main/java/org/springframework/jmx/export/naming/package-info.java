9805cf5f2ec92b8c884e40d7174e573c8a096c4b
John McDonald <john@threatrix.io>
2023-04-06 14:40:48 -0500

    update

--- clones/spring-framework-5.3.1/spring-context/src/main/java/org/springframework/jmx/export/naming/package-info.java
+++ clones/spring-framework-5.3.1/spring-context/src/main/java/org/springframework/jmx/export/naming/package-info.java
@@ -1,3 +1,26 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Project: https://spring.io/projects/spring-framework
+*    Release: https://github.com/spring-projects/spring-framework/releases/tag/v5.2.20.RELEASE
+*    Source File: package-info.java
+*    
+*    Copyrights:
+*      copyright 2002,2003,2004 the apache software foundation
+*      copyright 2003,2004 the apache software foundation
+*      copyright (c) 2000-2011 inria, france telecom
+*      copyright 2002-2018 the original author or authors
+*    
+*    Licenses:
+*      Apache License 2.0
+*      SPDXId: Apache-2.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * Provides a strategy for ObjectName creation. Used by MBeanExporter
  * to determine the JMX names to use for exported Spring-managed beans.
