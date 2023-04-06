6ee3870e0d94f01193e0dd4dacbacf913d47f172
John McDonald <john@threatrix.io>
2023-04-06 14:49:57 -0500

    update

--- clones/magento2-2.2.9/app/code/Magento/Ui/view/base/web/js/form/element/select.js
+++ clones/magento2-2.2.9/app/code/Magento/Ui/view/base/web/js/form/element/select.js
@@ -1,3 +1,25 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/gitmastro/magento2-custom-product/releases/tag/master-0e3994b
+*    Source File: select.js
+*    
+*    Copyrights:
+*      copyright © magento, inc. all rights reserved
+*    
+*    Licenses:
+*      Open Software License 3.0
+*      SPDXId: OSL-3.0
+*    
+*      Academic Free License v3.0
+*      SPDXId: AFL-3.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
