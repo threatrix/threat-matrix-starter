3b3f051d634a2cdb500233947139e6df9cf147e6
John McDonald <john@threatrix.io>
2023-04-06 14:25:30 -0500

    update

--- clones/magento2-2.2.9/lib/web/tiny_mce/classes/html/Styles.js
+++ clones/magento2-2.2.9/lib/web/tiny_mce/classes/html/Styles.js
@@ -1,3 +1,25 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/ZeeshanShafiq1161/testproject/releases/tag/master-ff34526
+*    Source File: Styles.js
+*    
+*    Copyrights:
+*      copyright 2010, moxiecode systems ab
+*    
+*    Licenses:
+*      Open Software License 3.0
+*      SPDXId: OSL-3.0
+*    
+*      Academic Free License v3.0
+*      SPDXId: AFL-3.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /**
  * Styles.js
  *
