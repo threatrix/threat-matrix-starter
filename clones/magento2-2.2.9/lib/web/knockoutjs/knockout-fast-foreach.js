b853b43dd9aa4b3b25df2e9928445ef080440134
John McDonald <john@threatrix.io>
2023-04-06 14:26:03 -0500

    update

--- clones/magento2-2.2.9/lib/web/knockoutjs/knockout-fast-foreach.js
+++ clones/magento2-2.2.9/lib/web/knockoutjs/knockout-fast-foreach.js
@@ -1,3 +1,25 @@
+/*
+*    ------ BEGIN LICENSE ATTRIBUTION ------
+*    
+*    Portions of this file have been appropriated or derived from the following project(s) and therefore require attribution to the original licenses and authors.
+*    
+*    Release: https://github.com/Sadhakkathoullah/magento-training-smile/releases/tag/master-90873a8
+*    Source File: knockout-fast-foreach.js
+*    
+*    Copyrights:
+*      (c) 2015
+*    
+*    Licenses:
+*      Open Software License 3.0
+*      SPDXId: OSL-3.0
+*    
+*      Academic Free License v3.0
+*      SPDXId: AFL-3.0
+*    
+*    Auto-attribution by Threatrix, Inc.
+*    
+*    ------ END LICENSE ATTRIBUTION ------
+*/
 /*!
   Knockout Fast Foreach v0.4.1 (2015-07-17T14:06:15.974Z)
   By: Brian M Hunt (C) 2015
